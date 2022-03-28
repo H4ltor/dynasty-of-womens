2207d933841e3f540d7ccb9b4cfcf37a2ae00f7f
H4ltor <timothee.cognard@ynov.com>
2022-03-28 23:18:57 +0200

    [Add] Root project & front video

--- apps/dynasty-of-womens/src/app/app.module.ts
+++ apps/dynasty-of-womens/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MainComponent } from './Component/public/main/main.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, MainComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
