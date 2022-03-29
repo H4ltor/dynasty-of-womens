e43e27902e11616231a973564ee5b8016c79a769
H4ltor <timothee.cognard@ynov.com>
2022-03-29 19:35:13 +0200

    [Modify] change template

---
+++ apps/dynasty-of-womens/src/app/Component/public/main/main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dynasty-of-womens-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent  {
+
+
+
+}
